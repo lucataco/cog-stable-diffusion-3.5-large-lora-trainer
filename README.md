681f5ba60ef6ff3cd1607c7c07020d7a6f660413
Luis C <me@lucataco.com>
2024-10-26 05:21:32 +0000

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Diffusers StableDiffusion3.5-Large LoRA Training Cog Model
+
+Cog implementation of the [Diffusers Dreambooth LoRA Trainer](https://github.com/huggingface/diffusers/blob/main/examples/dreambooth/README_sd3.md)
+
+## How to use
+
+Make sure you have [Cog](https://github.com/replicate/cog) installed.
+
+To run a training prediction:
+
+    cog predict -i input_images=@example.zip
+
+
+## Licensing and commercial use
+
+If you generate images on Replicate with StableDiffusion3.5 models and their fine-tunes, then you can use the images commercially.
+
+If you download the weights off Replicate and generate images on your own computer, you can’t use the images commercially.
---
+++ predict.py
@@ -0,0 +1,136 @@
+# Prediction interface for Cog ⚙️
+# https://cog.run/python
+
+from cog import BasePredictor, Input, Path, Secret
+import os
+import time
+import subprocess
+from zipfile import ZipFile
+
+MODEL_CACHE = "stable-diffusion-3.5-large"
+MODEL_URL_DEV = "https://weights.replicate.delivery/default/stabilityai/stable-diffusion-3.5-large/full.tar"
+
+def download_weights(url, dest):
+    start = time.time()
+    print("downloading url: ", url)
+    print("downloading to: ", dest)
+    subprocess.check_call(["pget", "-xf", url, dest], close_fds=False)
+    print("downloading took: ", time.time() - start)
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+        os.system("accelerate config default --mixed_precision bf16")
+        print("Loading SD3.5-Large pipeline")
+        if not os.path.exists(MODEL_CACHE):
+            download_weights(MODEL_URL_DEV, MODEL_CACHE)
+
+    # LoRA + DreamBooth
+    def predict(
+        self,
+        input_images: Path = Input(description="A zip file containing the images that will be used for training.", default=None,),
+        instance_prompt: str = Input(description="Instance prompt to trigger the image generation", default="Frog, yarn art style"),
+        resolution: int = Input(description="The resolution for input images, all the images in the train/validation dataset will be resized to this", default=768, choices=[512,768,1024]),
+        max_train_steps: int = Input(description="Total number of training steps to perform", default=700, ge=100, le=6000),
+        rank: int = Input(description="The dimension of the LoRA", default=16, ge=4, le=64),
+        train_batch_size: int = Input(description="Batch size for the training dataloader", default=1, ge=1, le=8),
+        gradient_accumulation_steps: int = Input(description="Number of updates steps to accumulate before performing a backward/update pass", default=1, ge=1, le=8),
+        optimizer: str = Input(description="The optimizer type to use", default="AdamW", choices=["AdamW", "prodigy"]),
+        learning_rate: float = Input(description="Initial learning rate to use (1.0 for Prodigy)", default=0.0001, ge=0.0001, le=1),
+        lr_scheduler: str = Input(description="'The scheduler type to use", default="constant", choices=["linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"]),
+        checkpointing_steps: int = Input(description="Save a checkpoint of the training state every X updates", default=None, ge=100, le=6000),
+        seed: int = Input(description="Seed for reproducibility", default=None),
+        backend: str = Input(description="Dynamo Backend", default="no", choices=["no", "eager", "aot_eager", "inductor", "nvfuser", "aot_nvfuser", "aot_cudagraphs", "ofi", "fx2trt", "onnxrt", "ipex"]),
+        hf_token: Secret = Input(description="Huggingface token (optional) with write access to upload to Hugging Face", default=None),
+        hub_model_id: str = Input(description="Huggingface model location for upload. Requires a HF token with write permissions. Ex: lucataco/SD3.5-Large-queso", default=None),
+        wandb_api_key: Secret = Input(description="Weights and Biases API key, if you'd like to log training progress to W&B.", default=None)
+    ) -> Path:
+        """Run a single prediction on the model"""
+        if seed is None:
+            seed = int.from_bytes(os.urandom(4), "big")
+        print(f"Using seed: {seed}")
+
+        # Cleanup
+        input_dir = "input_images"
+        output_base = "/tmp/train"
+        output_logs = "/tmp/logs"
+        output_dir = output_base + "/output/sd35_large_train_replicate"
+        os.system(f"rm -rf {input_dir}")
+        os.system(f"rm -rf {output_base}")
+        os.system(f"rm -rf {output_logs}")
+
+        # Check input images zip file
+        if not input_images.name.endswith(".zip"):
+            raise ValueError("input_images must be a zip file")
+        # Extract images from zip file
+        input_dir = Path(input_dir)
+        input_dir.mkdir(parents=True, exist_ok=True)
+        image_count = 0
+        with ZipFile(input_images, "r") as zip_ref:
+            for file_info in zip_ref.infolist():
+                if not file_info.filename.startswith(
+                    "__MACOSX/"
+                ) and not file_info.filename.startswith("._"):
+                    zip_ref.extract(file_info, input_dir)
+                    image_count += 1
+        print(f"Extracted {image_count} files from zip to {input_dir}")
+
+        # Dont save checkpoints by default
+        if checkpointing_steps is None:
+            checkpointing_steps = max_train_steps+1
+
+        # Check optimizer
+        if optimizer == "prodigy":
+            learning_rate = 1.0
+        # Trainer params
+        run_params = [
+            "accelerate",
+            "launch",
+            "--dynamo_backend", backend,
+            "train_dreambooth_lora_sd3.py",
+            "--pretrained_model_name_or_path", MODEL_CACHE,
+            "--instance_data_dir", str(input_dir),
+            "--rank", str(rank),
+            "--output_dir", output_dir,
+            "--mixed_precision", "bf16",
+            "--instance_prompt", instance_prompt,
+            "--resolution", str(resolution),
+            "--train_batch_size", str(train_batch_size),
+            "--gradient_accumulation_steps", str(gradient_accumulation_steps),
+            "--optimizer", str(optimizer),
+            "--learning_rate", str(learning_rate),
+            "--lr_scheduler", lr_scheduler,
+            "--lr_warmup_steps", "0",
+            "--max_train_steps", str(max_train_steps),
+            "--checkpointing_steps", str(checkpointing_steps),
+            "--seed", str(seed),
+            "--logging_dir", output_logs
+        ]
+
+        # Check to upload to HF
+        if hf_token is not None and hub_model_id is not None:
+            token = hf_token.get_secret_value()
+            os.system(f"huggingface-cli login --token {token}")
+            run_params.extend(["--push_to_hub"])
+            run_params.extend(["--hub_token", token])
+            run_params.extend(["--hub_model_id", hub_model_id])
+
+        # Check to log training run to Wandb
+        if wandb_api_key:
+            api_key = wandb_api_key.get_secret_value()
+            os.environ["WANDB_API_KEY"] = api_key
+            run_params.extend(['--report_to', 'wandb'])
+        
+        # Run the trainer
+        print(f"Using params: {run_params}")
+        subprocess.run(run_params, check=True, close_fds=False)
+
+        # Rename safetensors to lora_weights.safetensors
+        if os.path.exists(f"{output_dir}/pytorch_lora_weights.safetensors"):
+            os.system(f"mv {output_dir}/pytorch_lora_weights.safetensors {output_dir}/lora.safetensors")
+
+        # Create uploadable tar
+        output_path = "/tmp/trained_model.tar"
+        os.system(f"tar -cvf {output_path} -C {output_base} .")
+        return Path(output_path)
